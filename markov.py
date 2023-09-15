a2b45d178192e5d89a706aa7a2558e2298892e69
SCP3812 Wojak <mrisaacjgraham@gmail.com>
2023-09-15 15:58:59 +0000

    yes

--- markov.py
+++ markov.py
@@ -24,6 +24,13 @@ def Markovian(markov):
     k = 2
     sets_of_k_words = [ ' '.join(words[i:i+k]) for i, _ in enumerate(words[:-k]) ]
     sets_count = len(list(set(sets_of_k_words)))
+    print(sets_count)
+    print(len(distinct_words))
+    next_after_k_words_matrix = [[column for column in range(sets_count)] for row in range(len(distinct_words))]
+    
+    distinct_sets_of_k_words = list(set(sets_of_k_words))
+    k_words_idx_dict = {word: i for i, word in enumerate(distinct_sets_of_k_words)}
+
     word = words[random.randint(0, len(words)-1)]
     result = ""
 
@@ -36,23 +43,23 @@ def Markovian(markov):
             word = words[random.randint(0, len(words)-1)]
     return result
 
-    def sample_next_word_after_sequence(word_sequence, alpha = 0):
-        next_word_vector = next_after_k_words_matrix[k_words_idx_dict[word_sequence]] + alpha
-        likelihoods = next_word_vector/next_word_vector.sum()
+def sample_next_word_after_sequence(word_sequence, alpha = 0):
+    next_word_vector = next_after_k_words_matrix[k_words_idx_dict[word_sequence]] + alpha
+    likelihoods = next_word_vector/next_word_vector.sum()
     
-        return weighted_choice(distinct_words, likelihoods.toarray())
+    return weighted_choice(distinct_words, likelihoods.toarray())
 
-    def stochastic_chain(seed, chain_length=15, seed_length=2):
-        current_words = seed.split(' ')
-        if len(current_words) != seed_length:
-            raise ValueError(f'wrong number of words, expected {seed_length}')
-            sentence = seed
+def stochastic_chain(seed, chain_length=15, seed_length=2):
+    current_words = seed.split(' ')
+    if len(current_words) != seed_length:
+        raise ValueError(f'wrong number of words, expected {seed_length}')
+        sentence = seed
 
-        for _ in range(chain_length):
-            sentence+=' '
-            next_word = sample_next_word_after_sequence(' '.join(current_words))
-            sentence+=next_word
-            current_words = current_words[1:]+[next_word]
+    for _ in range(chain_length):
+        sentence+=' '
+        next_word = sample_next_word_after_sequence(' '.join(current_words))
+        sentence+=next_word
+        current_words = current_words[1:]+[next_word]
     return sentence
 
 # example use    
