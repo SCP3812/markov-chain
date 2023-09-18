6ed1960c39061a60b32a8505d360fe77a2205609
SCP3812 Wojak <mrisaacjgraham@gmail.com>
2023-09-18 21:53:29 +0000

    wojaL'

--- dataset.py
+++ dataset.py
@@ -1,6 +1,6 @@
 from corpus import load_corpus
 
 def dataset_init():
-    markov_list = load_corpus(["patricia_taxxon.txt", "ccru.txt"]).split(" ")
+    markov_list = load_corpus(["of_the_russe_common_wealth.txt", "fanged_noumena.txt"]).split(" ")
     markov_list= [word for word in markov_list if word != '']
     return markov_list
--- markov.py
+++ markov.py
@@ -42,5 +42,5 @@ def stochastic_chain(seed, chain_length, seed_length):
 # example use
 i = 0
 while i < 3:  
-    print(stochastic_chain("my father", 300, 2))
+    print(stochastic_chain("tartars that ", 300, 2))
     i = i + 1
