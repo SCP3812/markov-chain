407aaf4897278739c4baa43b51bfbbf66b7f5151
SCP3812 Wojak <mrisaacjgraham@gmail.com>
2025-07-24 00:12:17 +0000

    added original sources

--- dataset.py
+++ dataset.py
@@ -1,6 +1,6 @@
 from corpus import load_corpus
 
 def dataset_init():
-    markov_list = load_corpus(["frankenstein_radio_controls.txt", "exxon.txt", "fanged_noumena.txt", "designedsystem.txt", "siemens.txt"]).split(" ")
+    markov_list = load_corpus(["frankenstein_radio_controls.txt", "exxon.txt", "fanged_noumena.txt", "designedsystem.txt", "siemens.txt", "patricia_taxxon.txt"]).split(" ")
     markov_list= [word for word in markov_list if word != '']
     return markov_list
